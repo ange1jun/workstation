9dc26b3bd5cac477cdfd723a1baea88083dba45f
ange1jun <ange1jun@icloud.com>
2025-12-02 17:37:47 +0900

    update

--- spa-workstation-front/src/main.jsx
+++ spa-workstation-front/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './main.css'
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8888';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://172.30.1.70:8889';
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
