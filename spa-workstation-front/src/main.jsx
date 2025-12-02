5d4ff294d008b18d18667053a9ab4ecc472cb41e
ange1jun <ange1jun@icloud.com>
2025-12-02 10:42:22 +0900

    update

--- spa-workstation-front/src/main.jsx
+++ spa-workstation-front/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './main.css'
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8888';
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
